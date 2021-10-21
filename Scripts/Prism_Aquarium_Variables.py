825316c6ab3ca9130e4993856776222e0ecbe409
Yann Moriaud <yann@fatfi.sh>
2021-10-21 18:33:12 +0200

    Bump to v1.0.2

--- Scripts/Prism_Aquarium_Variables.py
+++ Scripts/Prism_Aquarium_Variables.py
@@ -33,7 +33,7 @@
 
 class Prism_Aquarium_Variables(object):
     def __init__(self, core, plugin):
-        self.version = "v1.0.0"
+        self.version = "v1.0.4"
         self.pluginName = "Aquarium Studio"
         self.pluginType = "ProjectManager"
         self.platforms = ["Windows", "Linux", "Darwin"]
