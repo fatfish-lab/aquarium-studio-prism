2e2a1b9b648049f4c7ed4253cb4e17d95791fabc
Yann Moriaud <yann@fatfi.sh>
2026-08-05 18:00:10 +0200

    fix: Ensure playlist names are correctly formatted when parent name is present

--- Scripts/Prism_Aquarium_Functions.py
+++ Scripts/Prism_Aquarium_Functions.py
@@ -1577,8 +1577,8 @@ class Prism_Aquarium_Functions(object):
 
             if "parent" in aqPlaylist:
                 if aqPlaylist.get("parent").get("_key") != self.aqProject._key:
-                    parentName = aqPlaylist.get("parent").get("data").get("name")
-                    playlist["name"] = f"{parentName}/{playlist["name"]}"
+                    parentName = aqPlaylist.get("parent").get("data").get("name") or ""
+                    playlist["name"] = f"{parentName}/{playlist.get("name", "")}"
 
             playlist["content"] = self.getContentOfPlaylist(aqPlaylist, allowCache=allowCache)
             playlists.append(playlist)
